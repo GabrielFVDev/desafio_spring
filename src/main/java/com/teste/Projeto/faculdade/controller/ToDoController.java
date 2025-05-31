11a312e0d98e7a7fbdb364bb630016b68da43a5b
Gabriel FV <gabrielvasco906@gmail.com>
2025-05-31 16:05:53 -0300

    last changes

--- src/main/java/com/teste/Projeto/faculdade/controller/ToDoController.java
+++ src/main/java/com/teste/Projeto/faculdade/controller/ToDoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*") // Allow all origins for CORS
 @RestController
 @RequestMapping("/api/todos")
 public class ToDoController {
